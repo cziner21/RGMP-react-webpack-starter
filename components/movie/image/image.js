713a1810fb7fe6a70808b01b5bad3a0a2e5af06e
cziner21 <cziner.adam@gmail.com>
2022-08-16 07:22:33 +0200

    Search page with working searchquery, images loading from image.tmdb.org

--- components/movie/image/image.js
+++ components/movie/image/image.js
@@ -4,14 +4,29 @@ import Image from 'next/image'
 
 const placeholder = '/150x200.png'
 
-const CoverImage = styled.img`
-    background-size: cover;
-    background-image: url(${(props) =>
-        props.imagePath ? props.imagePath : placeholder});
-    background-color: #232323;
-    width: 150px;
-    height: 200px;
-`
+const CoverImage = (props) => {
+    return (
+        <Image
+            style={{
+                backgroundSize: 'cover',
+                backgroundColor: '#232323',
+            }}
+            src={props.imagePath ? props.imagePath : placeholder}
+            alt={props.imagePath ? props.imagePath : 'placeholder'}
+            width={150}
+            height={200}
+        />
+    )
+}
+
+// const CoverImage = styled.img`
+//     background-size: cover;
+//     background-image: url(${(props) =>
+//         props.imagePath ? props.imagePath : placeholder});
+//     background-color: #232323;
+//     width: 150px;
+//     height: 200px;
+// `
 
 CoverImage.propTypes = {
     imagePath: PropTypes.string,
